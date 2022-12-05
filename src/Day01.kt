5455d1fcc567bf63a16534b3d82ae2f407b1d41c
Niels Marsman <niels.marsman@luminis.eu>
2022-12-05 17:51:19 +0100

    Advent of code - Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input.split("\n\n").maxOf { elf ->
+            elf.lines().sumOf { it.toInt() }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return input.split("\n\n")
+            .map { elf -> elf.lines().sumOf { it.toInt() } }
+            .sortedDescending()
+            .subList(0, 3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
